a760de7a373eca018e66aa16d8dededcbde9ea76
Julia Kreiner <julia.kreiner@mail.utoronto.ca>
2020-08-13 22:07:28 -0400

    Update EHHstats_plotting.R

--- plotting_n_summarizing_popgenstatsforGWA/EHHstats_plotting.R
+++ plotting_n_summarizing_popgenstatsforGWA/EHHstats_plotting.R
@@ -2,46 +2,53 @@
 ################################
 
 library(data.table)
-obs<-fread("EHH_5kwindowed_allTSRobserved.txt")
+obs<-fread("EHH_5kwindowed_allTSRobserved.txt") #read in 5000bp window EHHs
 
-obs<-obs %>% separate(V7, c("A", "B","chr","ps"))
+obs<-obs %>% separate(V7, c("A", "B","chr","ps")) #get scafs output SNP name
 obs$chr<-gsub('Scaf', '', obs$chr)
 obs$chr<-as.integer(as.character(obs$chr))
 obs$ps<-as.integer(as.character(obs$ps))
 
 obs$B<-gsub('rep', '', obs$B)
-names(obs)<-c("win","ehh1","ehh0","snps","start","end","gen","rep","chr","ps")
-se <- function(x) sqrt(var(x)/length(x))
+names(obs)<-c("win","ehh1","ehh0","snps","start","end","gen","rep","chr","ps") #name columns
+se <- function(x) sqrt(var(x)/length(x)) #define SE
+
+#get windows 100kb around focal snps
 filted<-obs %>% filter(win < 11 & win > -11) %>% group_by(win) %>% summarise(EHH1_mean=mean(ehh1),EHH0_mean=mean(ehh0),EHH1_SE=se(ehh1),EHH0_SE=se(ehh0))
+
+#change to long format
 ehh1<-filted[,c(1,2,4)]
 ehh1$class<-"EHH1"
 ehh0<-filted[,c(1,3,5)]
 ehh0$class<-"EHH0"
 
-
+#read in GWAS output
 sex<-fread("~/forgwas_nodups_tryagain_updated_indsexcluded_TSR.assoc.txt",header=T)
 sex$FDR_corr<-p.adjust(sex$p_lrt,method = "fdr")
 sig<-sex[sex$FDR_corr<0.05,]
 sig_neg<-sig[sig$beta<0,]
 
+#swap 0,1 alleles for significant hit if negative effect on resistance 
 neg_effects<-semi_join(obs,sig_neg,by=c("chr","ps"))
 pos_effects<-anti_join(obs,sig_neg,by=c("chr","ps"))
 neg_swapped<-neg_effects[,c(1,3,2,4,5,6,7,8,9,10)]
 obs<-rbind(pos_effects,neg_swapped)
 
-###
+###do the sanme for permuted
 sex<-fread("~/forgwas_nodups_tryagain_updated_indsexcluded_TSR.assoc.txt",header=T)
 hdr<-names(sex)
 sex<-read.table("TSR_sigSNPS_frompermutation2.txt",header = F)
 names(sex)<-c("itt",hdr)
 sig_neg<-sex[sex$beta<0,]
 
+#EHH values from empirical null sig hits
 exp<-fread("EHH_5kwindowed_allTSRpermuted2.txt")
 exp<-exp %>% separate(V7, c("A", "B","chr","ps"))
 exp$chr<-gsub('Scaf', '', exp$chr)
 exp$chr<-as.integer(as.character(exp$chr))
 exp$ps<-as.integer(as.character(exp$ps))
 
+#swap
 neg_effects<-semi_join(exp,sig_neg,by=c("chr","ps"))
 pos_effects<-anti_join(exp,sig_neg,by=c("chr","ps"))
 neg_swapped<-neg_effects[,c(1,3,2,4,5,6,7,8,9,10)]
@@ -50,7 +57,7 @@ names(exp)<-c("win","ehh1","ehh0","snps","start","end","gen","rep","chr","ps")
 
 se <- function(x) sqrt(var(x)/length(x))
 
-#filter(win < 11 & win > -11)
+#get windows
 filted<-obs %>%  group_by(win) %>% summarise(EHH1_mean=mean(ehh1),EHH0_mean=mean(ehh0),EHH1_SE=se(ehh1),EHH0_SE=se(ehh0),n_win=n())
 exp2<- exp %>% group_by(win) %>% summarise(EHH1_mean=mean(ehh1),EHH0_mean=mean(ehh0),EHH1_SE=se(ehh1),EHH0_SE=se(ehh0),n_win=n())
 nonscaf<-obs %>%  filter(gen != "Scaf5") %>% group_by(win) %>% summarise(EHH1_mean=mean(ehh1),EHH0_mean=mean(ehh0),EHH1_SE=se(ehh1),EHH0_SE=se(ehh0),n_win=n())
@@ -63,6 +70,7 @@ exp2$Class<-"Permuted"
 filted$Class<-"Observed"
 nonscaf$Class<-"Non-Scaf5 Obs."
 
+#join datasets for plotting
 both<-rbind(exp2,filted)
 all<-rbind(nonscaf,both)
 
@@ -78,12 +86,12 @@ ehh1_2$upper<-ehh1_2$ehh+ehh1_2$se
 ehh1_2$lower<-ehh1_2$ehh-ehh1_2$se
 
 #long$Class <- factor(ehh1_2$class, levels=c("Genome-wide","Permuted","Observed","Non-Scaf5 Obs."))
-
+#color scheme
 library(PNWColors)
 bay<-pnw_palette("Bay",8,type="continuous")
 three<-c(bay[8],bay[6],bay[3],"#deafa6","#fcd392","#b5e9eb")
 
-
+#plot
 ehh1_2 %>% 
   ggplot(aes(wind,ehh,color=interaction(class,allele))) +
   geom_point() +
@@ -103,15 +111,16 @@ ehh1_2 %>%
 ###############
 head(obs)
 
-obs_test<- obs[obs$win < -1 | obs$win > 1,]
-exp_test<- exp[exp$win < -1 | exp$win > 1,]
+#obs_test<- obs[obs$win < -1 | obs$win > 1,] #check 1kb around
+#exp_test<- exp[exp$win < -1 | exp$win > 1,] #check 1kb around
 
 #%>% filter(win < 11 & win > -11 ) 
-obs_avg<-obs_test %>% group_by(chr,ps) %>% summarise(EHH1_mean=mean(ehh1),EHH2_mean=mean(ehh0),EHH1_SE=se(ehh1),EHH2_SE=se(ehh0))
-exp_avg<- exp_test %>% group_by(chr,ps) %>% summarise(EHH1_mean=mean(ehh1),EHH2_mean=mean(ehh0),EHH1_SE=se(ehh1),EHH2_SE=se(ehh0))
+obs_avg<-obs_test %>% filter(win < 11 & win > -11 ) %>% group_by(chr,ps) %>% summarise(EHH1_mean=mean(ehh1),EHH2_mean=mean(ehh0),EHH1_SE=se(ehh1),EHH2_SE=se(ehh0))
+exp_avg<- exp_test %>% filter(win < 11 & win > -11 ) %>% group_by(chr,ps) %>% summarise(EHH1_mean=mean(ehh1),EHH2_mean=mean(ehh0),EHH1_SE=se(ehh1),EHH2_SE=se(ehh0))
 x=nrow(exp_avg)
 y=nrow(obs_avg)
 
+#resample w replacement to n(obs.GWAS) 1000 times
 perm_resample<-list(); perm_mean1<-list();  perm_mean0<-list();
 for (i in 1:1000) {
   interm<-exp_avg[sample(x,y,replace = T),]
@@ -121,15 +130,15 @@ for (i in 1:1000) {
   
 }
 
-perm_resample_df<-do.call(rbind, perm_resample)
+perm_resample_df<-do.call(rbind, perm_resample) 
 perm_stats<-as.data.frame(do.call(rbind, perm_mean1))
 perm_stats$perm_mean2<-unlist(perm_mean0)
 
 names(perm_stats)<-c("mean_EHH1","mean_EHH2")
 
-quantile(perm_stats$mean_EHH1,probs = c(0.05, 0.95))
-median(obs_avg$EHH1_mean)
+quantile(perm_stats$mean_EHH1,probs = c(0.05, 0.95)) #median EHH1 95% CIs
+median(obs_avg$EHH1_mean) #median EHH1 obs
 
-quantile(perm_stats$mean_EHH2,probs = c(0.05, 0.95))
-median(obs_avg$EHH2_mean)
+quantile(perm_stats$mean_EHH2,probs = c(0.05, 0.95)) #median EHH2 95% CIs
+median(obs_avg$EHH2_mean)  #median EHH2 obs
 
