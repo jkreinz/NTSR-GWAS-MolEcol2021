7b5c0417f80f6ef7273733101a9d7fcf63f84aa8
Julia Kreiner <julia.kreiner@mail.utoronto.ca>
2020-11-19 16:06:48 -0500

    Update parallel_permute_n_fst.sh

--- GWA_permuations/parallel_permute_n_fst.sh
+++ GWA_permuations/parallel_permute_n_fst.sh
@@ -6,7 +6,7 @@ do
 	awk '$6 >= 2' TSR_rep_${i}/forgwas_nodups_tryagain_updated_indsexcluded.fam | cut -d " " -f 1 > res_inds_${i} #list interations resistant inds
 	awk '$6 < 2' TSR_rep_${i}/forgwas_nodups_tryagain_updated_indsexcluded.fam | cut -d " " -f 1 > sus_inds_${i} #list of interations sus inds
 
-	vcftools --vcf TSR_permutedsigsites.recode.vcf --weir-fst-pop TSR_rep_${i}/res_inds --weir-fst-pop res_inds_${i} --weir-fst-pop sus_inds_${i} --out TSR_permutedsigsites_RvS_${i}
+	vcftools --vcf TSR_permutedsigsites.recode.vcf --weir-fst-pop res_inds_${i} --weir-fst-pop sus_inds_${i} --out TSR_permutedsigsites_RvS_${i}
 	#calculate weir cockerham fst
 done
 
