ab44fafa80cb296296295c28e3db0a0c4f4b890e
Julia Kreiner <julia.kreiner@mail.utoronto.ca>
2020-12-18 12:53:20 -0500

    Update parallel_permute_n_fst.sh

--- GWA_permuations/parallel_permute_n_fst.sh
+++ GWA_permuations/parallel_permute_n_fst.sh
@@ -11,3 +11,8 @@ do
 done
 
 cat *fst | grep -v "CHROM" > TSR_permutedsigsites_RvS.weir.fst #combine fst estimates for all sites across all iterations to estimate empirical null expectation
+
+
+
+
+for i in {1..250}; do awk '$6 > 0' ../Binarypop_rep_${i}/forgwas_nodups_tryagain_updated_indsexcluded.fam | awk '{print $1 "\t" $2}' | sed -e's/K\t/K/g' | sed -e's/D\t/D/g' | sed -e's/B\t/B/g' | sed -e's/H\t/H/g' | sed -e's/E\t/E/g' | sed -e's/F\t/F/g' | sed -e's/G\t/G/g' | sed -e's/J\t/J/g' | tr "\t" "_" > res_inds_${i}; awk '$6 < 1' ../Binarypop_rep_${i}/forgwas_nodups_tryagain_updated_indsexcluded.fam | awk '{print $1 "\t" $2}' | sed -e's/K\t/K/g' | sed -e's/D\t/D/g' | sed -e's/B\t/B/g' | sed -e's/H\t/H/g' | sed -e's/E\t/E/g' | sed -e's/F\t/F/g' | sed -e's/G\t/G/g' | sed -e's/J\t/J/g' | tr "\t" "_" > sus_inds_${i}; grep "^$i    " ../binary_FDR05_sigsnps.txt | awk '{print $2 "\t" $4}' > rep_${i}_pos; vcftools --vcf ../binary_FDR05_sigsnps.recode.vcf --weir-fst-pop res_inds_${i} --weir-fst-pop sus_inds_${i} --out binary_FDR05_permutedsigsites_RvS_${i} --positions rep_${i}_pos; done
