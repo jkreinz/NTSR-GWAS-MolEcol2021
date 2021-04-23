7195c02a7a3354c243fb9f1918cdd19a626680db
Julia Kreiner <julia.kreiner@mail.utoronto.ca>
2021-04-23 15:11:53 -0700

    Update getSigPermutedGWAhits_nFDR.sh

--- GWA_permuations/getSigPermutedGWAhits_nFDR.sh
+++ GWA_permuations/getSigPermutedGWAhits_nFDR.sh
@@ -6,6 +6,9 @@ for i in {1..250}; do awk -v var="$i" '($14 < 0.000000402) {print var "\t" $0}' 
 for i in {1..250}; do awk -v var="$i" '($14 < 0.000001102) {print var "\t" $0}' forgwas_nodups_tryagain_updated_indsexcluded_TSR_${i}.assoc.txt; done >> TSR_sigSNPS_frompermutation.txt &
 #value is equivalent to pvalue cutoff at q < 0.05
 
+#for +TSR+EPSPS model
+for i in {1..250}; do awk -v var="$i" '($14 < 0.00000251188) {print var "\t" $0}' forgwas_nodups_tryagain_updated_indsexcluded_twocovs_${i}.assoc.txt; done >> twocovs_sigSNPS_frompermutation.txt &
+#value is equivalent to pvalue cutoff at q < 0.05
 
 ########
 #read into R and calculate number of average # significant hits per GWAS (estimated FDR)
@@ -20,8 +23,8 @@ counts_byitt<-table(TSR_sigs$itt) #table of number significant hits @ q < 0.05 a
 ittcounts<-as.vector(counts_byitt)
 ittcounts<-c(ittcounts,rep(0,(250-length(ittcounts)))) #add 0's for however many GWAS itts had no sig hits
 
-average_sighits_perGWA<-mean(ittcounts) #get mean
-median_sighits_perGWA<-median(ittcounts) # get median
+average_sighits_perGWA<-mean(ittcounts) #get mean FDR
+median_sighits_perGWA<-median(ittcounts) # get median FDR
 
 #######################
 
